f87db711818a3bab0661c1fa8ab89b66d633ad1f
Marco Ratto <marco.ratto@jrc.ec.europa.eu>
2011-12-20 16:16:45 +0000

    added hidden utility to close slave upon closing matlab.
    git-svn-id: svn://localhost/parallel-svn-basic@12 0ec165a9-8f43-a94c-a5c0-4a10344b24dc

---
+++ Exit from Parallel/finish.m
@@ -0,0 +1,18 @@
+% Scrip used to close, if exist
+% the slaves. This code must be execut onfly by "master2 Matalab!
+
+try
+    if isfield(Parallel_info,'RemoteTmpFolder')
+        s=warning('off');
+        closeSlave(Parallel, Parallel_info.RemoteTmpFolder);
+        disp(' ');
+        disp('Slave(s), closed!');
+        disp(' ');
+        s=warning('on');
+    end
+catch
+    s=warning('on');
+    disp(' ');
+    disp('Some error(s)occurred while try to close the "Slaves"!');
+    disp(' ');
+end
